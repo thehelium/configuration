49685d46f9dc0c0e92378c21fd682736886a7b83
Harris <rickoneget@gmail.com>
2026-04-24 09:49:00 +0800

    [nix] add authorized SSH keys for harris

--- nix/hosts/neon/users.nix
+++ nix/hosts/neon/users.nix
@@ -9,6 +9,10 @@
       "render"
     ];
     shell = pkgs.zsh;
+    # add your own public key here to enable passwordless login
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICh/J0HfRoQ6sRkckkt2jV1zssTVQN1BQu8AgovlTVcs harris@macos"
+    ];
   };
 
   programs.zsh.enable = true;
