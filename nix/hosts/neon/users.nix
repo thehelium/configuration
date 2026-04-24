01af816b2ade53220a78b636e1e275662c5c5b15
Harris <rickoneget@gmail.com>
2026-04-24 10:00:38 +0800

    [nix] add authorized SSH keys

--- nix/hosts/neon/users.nix
+++ nix/hosts/neon/users.nix
@@ -12,6 +12,7 @@
     # add your own public key here to enable passwordless login
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICh/J0HfRoQ6sRkckkt2jV1zssTVQN1BQu8AgovlTVcs harris@macos"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFZedVboReAqHpWaexnFYMtv4PyMueczro8a/jMpNAuW harris@neon"
     ];
   };
 
